b8fae24117ca50f9e61d71e3d641b27303481323
Denis <denis.petrov56245@gmail.com>
2023-12-19 17:07:41 +0100

    all task done

--- src/main/java/com/example/TS/Note.java
+++ src/main/java/com/example/TS/Note.java
@@ -1,9 +1,21 @@
 package com.example.TS;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 
 
@@ -29,6 +41,22 @@ public class Note {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
     @Override
--- src/main/java/com/example/TS/NoteService.java
+++ src/main/java/com/example/TS/NoteService.java
@@ -2,73 +2,44 @@ package com.example.TS;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class NoteService {
 
-    private final Map<Long, Note> noteMap = new HashMap<>();
+//    private final Map<Long, Note> noteMap = new HashMap<>();
+
+    private final NoteRepository repository;
     private final AtomicLong idCounter = new AtomicLong();
 
-//    private long idCounter = 1;
-
-//    public List<Note> listAll() {
-//        List<Note> allNotesAsString = new ArrayList<>();
-//        for (Note note : noteMap.values()) {
-//            allNotesAsString.add(note);
-//        }
-//        return allNotesAsString;
-//    }
-
-//    public Note add(Note note) {
-//        long generatedId = idCounter++;
-//        note.setId(generatedId);
-//        noteMap.put(generatedId, note);
-//        return note;
-//    }
+    public NoteService(NoteRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Note> listAll() {
-        return new ArrayList<>(noteMap.values());
+        return repository.findAll();
     }
 
     public Note add(Note note) {
-        long generatedId = idCounter.incrementAndGet();
-        note.setId(generatedId);
-        noteMap.put(generatedId, note);
-        return note;
+        note.setCreatedAt(LocalDateTime.now());
+        note.setUpdatedAt(LocalDateTime.now());
+        return repository.save(note);
     }
+
 
     public void deleteById(long id) {
-        if (!noteMap.containsKey(id)) {
-            throw new IllegalArgumentException("Note with id " + id + " not found");
-        }
-        noteMap.remove(id);
+        repository.deleteById(id);
     }
 
-//    public void update(Note note) {
-//        long id = note.getId();
-//        if (!noteMap.containsKey(id)) {
-//            throw new IllegalArgumentException("Note with id " + id + " not found");
-//        }
-//        noteMap.put(id, note);
-//    }
-
     public void update(Note note) {
-        Long id = note.getId();
-        if (id == null || !noteMap.containsKey(id)) {
-            throw new IllegalArgumentException("Invalid note or note with id " + id + " not found");
-        }
-        noteMap.put(id, note);
+        note.setUpdatedAt(LocalDateTime.now());
+        repository.save(note);
     }
 
     public Note getById(long id) {
-        if (!noteMap.containsKey(id)) {
-            throw new IllegalArgumentException("Note with id " + id + " not found");
-        }
-        return noteMap.get(id);
+        return repository.findById(id).orElse(null);
     }
 }
